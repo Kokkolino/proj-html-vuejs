ffb162d48df3129bcb6e593b1fa67127cae9c3f6
Francesco C <fran461j@outlook.it>
2022-12-02 13:06:07 +0100

    starter

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <HeaderComp/>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.w-100{
+  width: 100%;
+}
+
+.w-70{
+  width: 70%;
+  margin: auto;
+}
+
+.h-100{
+  height: 100%;
+}
+
+// flex
+.f-bw{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="w-100">
+    <nav class="w-70 h-100 f-bw">
+      <img id="logo" src="../assets/img/logo.png" alt="logo">
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          Pages
+        </li>
+        <li>
+          Blog
+        </li>
+        <li>
+          Shop
+        </li>
+        <li>
+        Shortcodes
+        </li>
+        <li>
+          Support
+        </li>
+        <li>
+          Contact
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+ 
+}
+</script>
+
+<style scoped lang="scss">
+  div{
+    height: 100px;
+  }
+
+  #logo{
+    height: 80px;
+  }
+
+  ul{
+    display: flex;
+    list-style: none;
+  }
+
+  li{
+    margin: 0 10px;
+  }
+</style>
